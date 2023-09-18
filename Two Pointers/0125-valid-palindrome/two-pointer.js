f650128cae7984d18253f09cbbd82174bb71ea78
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-09-19 00:40:23 +0530

    Update two-pointer.js

--- Two Pointers/0125-valid-palindrome/two-pointer.js
+++ Two Pointers/0125-valid-palindrome/two-pointer.js
@@ -10,10 +10,10 @@
 var isPalindrome = function(s) {
  s =  s.toLowerCase().replace(/[^a-z0-9]/g,'');
  let left = 0;
- let right = str.length-1;
+ let right = s.length-1;
 
  while(left<=right) {
-   if(str[left] !== str[right]) return false;
+   if(s[left] !== s[right]) return false;
    left++;
    right--;
  }
