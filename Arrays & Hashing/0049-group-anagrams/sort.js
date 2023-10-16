cdfe866c7675ba5500514e15fdaf40e39073ed1b
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-10-16 23:10:24 +0530

    Update sort.js

--- Arrays & Hashing/0049-group-anagrams/sort.js
+++ Arrays & Hashing/0049-group-anagrams/sort.js
@@ -1,1 +1,19 @@
-
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const res = []
+    const obj = {}
+    let sorted = ''
+    for(let i=0;i<strs.length;i++) {
+        sorted = strs[i].split('').sort().join('')
+        if (sorted in obj) obj[sorted].push(strs[i])
+        else obj[sorted] = [strs[i]]
+    }
+    const valuesArr = Object.values(obj)
+    for (const item of valuesArr) {
+        res.push(item)
+    }
+    return res;
+};
