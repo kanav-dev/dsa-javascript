d3ac19f3d1200fd47c3a9124ff96c0bd40093abf
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-10-17 12:19:48 +0530

    Create brute-force.js

---
+++ Sliding Window/0121-best-time-to-buy-and-sell-stock/brute-force.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Two Pointer
+ * Time Complexity:  O(N^2) 
+ * Space Complexity: O(1)
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function(prices) {
+  let maxPr = 0;
+  
+  for(let i=0;i<prices.length;i++) {
+    for(let j=i+1;j<prices.length;j++) {
+      maxPr = Math.max(maxPr, prices[j]-prices[i]);
+    }
+  }
+  
+  return maxPr;
+};
