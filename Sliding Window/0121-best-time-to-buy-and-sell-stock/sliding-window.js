1e1eeadf1b438d001acb1d4e824514a7d775ba03
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-10-17 12:22:37 +0530

    Create sliding-window.js

---
+++ Sliding Window/0121-best-time-to-buy-and-sell-stock/sliding-window.js
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Two Pointer
+ * Time Complexity:  O(N) 
+ * Space Complexity: O(1)
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function(prices) {
+  let maxPr = 0;
+  let left = 0;
+  let right = 1;
+
+  while (right<prices.length) {
+    if (prices[left] >= prices[right]) {
+      left = right;
+      right++;
+      continue;
+    }
+    const profit = prices[right] - prices[left];
+    maxPr = Math.max(maxPr, profit);
+    right++;
+  }
+  
+  return maxPr;
+};
