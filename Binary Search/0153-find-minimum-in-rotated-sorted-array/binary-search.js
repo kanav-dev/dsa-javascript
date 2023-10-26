09533ea8d7655ca7f8c9c5fcdc7f4d443c6ff90e
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-10-26 23:33:38 +0530

    Update binary-search.js

--- Binary Search/0153-find-minimum-in-rotated-sorted-array/binary-search.js
+++ Binary Search/0153-find-minimum-in-rotated-sorted-array/binary-search.js
@@ -12,7 +12,7 @@ var findMin = function(nums) {
   let right = nums.length-1
 
   while(left<=right) {
-    mid = Math.floor((left+right)/2)
+    const mid = Math.floor((left+right)/2)
     if(nums[left] <= nums[right]) return nums[left];
     if(nums[mid]>=nums[left]) left = mid+1
     else right = mid
