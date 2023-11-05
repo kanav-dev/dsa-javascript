74afbc107ece2dd24953b0f5c766cbea858d2920
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-11-05 22:21:47 +0530

    Update index.js

--- Sorting/bubble-sort/index.js
+++ Sorting/bubble-sort/index.js
@@ -7,14 +7,14 @@
  */
 
 const bubbleSort = (arr) => {
-	for (let i=0;i<arr.length;i++) {
-		for(let j=0;j<arr.length-1-i;j++) {
-			if(arr[j]>arr[j+1]) {
-				const temp = arr[j+1]
-				arr[j+1]=arr[j]
-				arr[j]=temp
-			}
-		}
-	}
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
   return arr;
 }
