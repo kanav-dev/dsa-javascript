24b057d958cc404b786808fa6905732208bfec89
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-11-05 22:20:10 +0530

    Create index.js

---
+++ Sorting/bubble-sort/index.js
@@ -0,0 +1,20 @@
+/**
+ * Bubble Sort is a comparison sort, named for the way the larger elements "bubble" up to the top of the list.
+ * Time Complexity (Best)    :  O(N) 
+ * Time Complexity (Average) :  O(N^2) 
+ * Time Complexity (Worst)   :  O(N^2)
+ * Space Complexity          :  O(1)
+ */
+
+const bubbleSort = (arr) => {
+	for (let i=0;i<arr.length;i++) {
+		for(let j=0;j<arr.length-1-i;j++) {
+			if(arr[j]>arr[j+1]) {
+				const temp = arr[j+1]
+				arr[j+1]=arr[j]
+				arr[j]=temp
+			}
+		}
+	}
+  return arr;
+}
